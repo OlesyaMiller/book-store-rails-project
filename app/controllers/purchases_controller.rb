d6f7c02eb8248472cc215ddf5562ad7b0098f54b
Olesya Miller <olesyam.miller@gmail.com>
2021-10-13 21:21:38 -0400

    get rid of purchase new action and view, remove unnecessary code

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,17 +2,6 @@ class PurchasesController < ApplicationController
 
     before_action :require_login
 
-    def new
-        @purchase = Purchase.new 
-        if params[:user_id]
-            user = User.find(params[:user_id])
-            @purchase.user = user 
-        else
-            user = User.find_by(id: current_user.id)
-            @purchase.user = user 
-        end
-    end
-
     def create
         purchase = Purchase.create(purchase_params)
         message = purchase.purchase_book 
